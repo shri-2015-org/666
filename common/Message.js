829ceb120d1112e1060b1c9a202932d49b6fd7b3
Vladimir Semyonov <tempname011@gmail.com>
2015-09-19 19:59:07 +0300

    Причесал User и Message

--- common/Message.js
+++ common/Message.js
@@ -1,6 +1,6 @@
 /**
  * Создает экземпляр Message
- * @constructor
+ * @class
  * @this {Message}
  * @param {Object} options - хэш с параметрами создаваемого экземпляра
  * @param {string} options.uid - идентификатор пользователя-отправителя
@@ -10,13 +10,14 @@
  * @param {string=} options.mid - идентификатор сообщения
  * @param {string=} options.status - статус сообщения
  */
-const Message = function Message(options) {
-  this.uid = options.uid;
-  this.mid = options.mid;
-  this.text = options.text;
-  this.time = options.time || Date.now();
-  this.read = options.read || false;
-  this.status = options.status;
-};
+export default class Message {
+  constructor(options) {
+    this.uid = options.uid;
+    this.mid = options.mid;
+    this.text = options.text;
+    this.time = options.time || Date.now();
+    this.read = options.read || false;
+    this.status = options.status;
+  }
+}
 
-module.exports = Message;
