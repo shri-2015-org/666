a7bf9dcdfb91f864ab3517d3f0a0d0d06ebe153d
Dmitry Akimov <deemidroll@gmail.com>
2015-09-19 15:58:05 +0300

    move gender to constant

--- server/userGenerator.js
+++ server/userGenerator.js
@@ -2,9 +2,10 @@ import _ from 'lodash';
 
 const FIRST = ['Limited', 'Endless', 'Orange', 'Blue', 'Swollen', 'Horrible', 'Great', 'Awesome', 'Terrible', 'Idiotic', 'Massive', 'Cheesy'];
 const LAST = ['Peppermints', 'Oil', 'Dragon', 'Cafeteria', 'Junkie', 'Vegetables', 'Twinkies', 'Turtle', 'Fox', 'Calculator', 'Bandwidth', 'Crust', 'Cake', 'Derp'];
+const GENDER = ['male', 'female'];
 
 function _generateUrl(uid) {
-  const gender = _.sample(['male', 'female']);
+  const gender = _.sample(GENDER);
   return `http://eightbitavatar.herokuapp.com/?id=${uid}&s=${gender}&size=64`;
 }
 /**
