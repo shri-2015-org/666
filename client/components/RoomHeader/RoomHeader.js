a14e31087458773194a0611953716f93b3c66031
John Grishin <r@exah.ru>
2015-09-19 18:40:18 +0300

    No more * + * :wink:

--- client/components/RoomHeader/RoomHeader.js
+++ client/components/RoomHeader/RoomHeader.js
@@ -13,17 +13,19 @@ export default class RoomHeader extends Component {
 
     return (
       <header className="room-header">
-        <div onClick={e => this.collapseNavigation(e, dispatch)} className="room-header-bar">
-            <span className="btn-icon">{this.props.collapsed ? '←' : '→'}</span>
-            <h3 className="room-name"><a href="#!/room/#frontend">#frontend</a></h3>
-        </div>
-        <div className="room-header-bar">
-          <h4 className="user-name">UserName</h4>
-          <div className="user-ava ava"
-           style={{'background-color': '#73DD40'}}>
-            <img src="media/icons/airplane.svg" alt="" />
-          </div>
-        </div>
+        <ul onClick={e => this.collapseNavigation(e, dispatch)} className="room-header-bar">
+          <li><span className="btn-icon">{this.props.collapsed ? '←' : '→'}</span></li>
+          <li><h3 className="room-name"><a href="#!/room/#frontend">#frontend</a></h3></li>
+        </ul>
+        <ul className="room-header-bar">
+          <li><h4 className="user-name">UserName</h4></li>
+          <li>
+            <div className="user-ava ava"
+             style={{'background-color': '#73DD40'}}>
+                <img src="media/icons/airplane.svg" alt="" />
+            </div>
+          </li>
+        </ul>
       </header>
     );
   }
