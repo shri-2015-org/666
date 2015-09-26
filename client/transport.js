b0585b5fefade5da189805c85b304667ae6bb40d
pofigizm <pofigizm@gmail.com>
2015-09-26 22:25:50 +0300

    exchange API wrappers in transport.js

--- client/transport.js
+++ client/transport.js
@@ -2,14 +2,8 @@ import io from 'socket.io-client';
 
 /* eslint no-console: 0 */
 const socket = io( window.location.hostname + ':' + DATAPORT );
+const EXCHANGE_TIMEOUT = 3000;
 console.log('transport: socket connection');
-
-function emit(type, data) {
-  const sendData =  Object.assign({ uid: _getUID() }, data);
-
-  console.log('transport: Emit:', type, sendData);
-  socket.emit(type, sendData);
-}
 
 function assert(condition) {
   if (!condition) {
@@ -59,4 +53,93 @@ export function onLeaveUser(handler) {
   });
 }
 
+let exchangeCount = 0;
+function getExchangeID() {
+  return exchangeCount++;
+}
+
+export function joinRoom(roomID) {
+  const exchangeID = getExchangeID();
+  socket.emit('client-request:joinRoom', {
+    exchangeID,
+    data: {
+      roomID,
+    },
+  });
+
+  return new Promise( (resolve, reject) => {
+    socket.once(`server-response:joinRoom@${exchangeID}`, res => {
+      assert(res instanceof Object);
+      assert(res.status === 'OK');
+      assert(res.data instanceof Object);
+      assert(res.data.identity instanceof Object);
+      assert(typeof res.data.identity.userID === 'string');
+      assert(typeof res.data.identity.avatar === 'string');
+      assert(typeof res.data.identity.nick === 'string');
+      assert(typeof res.data.identity.secret === 'string');
+      assert(res.data.room instanceof Object);
+      assert(typeof res.data.room.roomID === 'string');
+      assert(typeof res.data.room.name === 'string');
+      assert(res.data.room.users instanceof Array);
+      res.data.room.users.forEach(user => {
+        assert(typeof user.roomID === 'string');
+        assert(typeof user.userID === 'string');
+        assert(typeof user.avatar === 'string');
+        assert(typeof user.nick === 'string');
+      });
+      resolve(res.data);
+    });
+    setTimeout(() => { reject('joinRoom timeout'); }, EXCHANGE_TIMEOUT);
+  });
+}
+
+export function leaveRoom({roomID, userID, secret}) {
+  const exchangeID = getExchangeID();
+  socket.emit('client-request:leaveRoom', {
+    exchangeID,
+    data: {
+      roomID,
+      userID,
+      secret,
+    },
+  });
+
+  return new Promise( (resolve, reject) => {
+    socket.once(`server-response:joinRoom@${exchangeID}`, res => {
+      assert(res instanceof Object);
+      assert(res.status === 'OK');
+      resolve();
+    });
+    setTimeout(() => { reject('leaveRoom timeout'); }, EXCHANGE_TIMEOUT);
+  });
+}
+
+export function message({roomID, userID, secret, text, time}) {
+  const exchangeID = getExchangeID();
+  socket.emit('client-request:message', {
+    exchangeID,
+    data: {
+      roomID,
+      userID,
+      secret,
+      text,
+      time,
+    },
+  });
+
+  return new Promise( (resolve, reject) => {
+    socket.once(`server-response:message@${exchangeID}`, res => {
+      assert(res instanceof Object);
+      assert(res.status === 'OK');
+      assert(res.data instanceof Object);
+      assert(typeof res.data.roomID === 'string');
+      assert(typeof res.data.userID === 'string');
+      assert(typeof res.data.messageID === 'string');
+      assert(typeof res.data.text === 'string');
+      assert(typeof res.data.time === 'number');
+      resolve(res.data);
+    });
+    setTimeout(() => { reject('message timeout'); }, EXCHANGE_TIMEOUT);
+  });
+}
 
