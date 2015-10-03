494f6d8663093705d41f5960665db289b7963bed
Denis Iogansen <pofigizm@gmail.com>
2015-10-03 23:24:53 +0300

    Second try 😥

--- server/transport.js
+++ server/transport.js
@@ -1,112 +1,128 @@
-/* eslint no-console: 0 */
 import http from 'http';
 import socketIO from 'socket.io';
 
-import actions from './mock/wrapper';
+import pureActions from './mock/wrapper';
 
 const socketServer = new http.Server();
-const io = socketIO(socketServer);
+const globalIO = socketIO(socketServer);
 
-const updateTop = () => {
+function createRoom(resEvents, res, socket) {
+  socket.emit(resEvents.exchange, {
+    status: 'OK',
+  });
+}
+
+function joinRoom(resEvents, res, socket, io) {
+  const { roomID } = res.room;
+  const { userID, nick, avatar } = res.identity;
+  const channel = `room:${roomID}`;
+
+  socket.join(channel);
+  socket.emit(resEvents.exchange, {
+    status: 'OK',
+    data: res,
+  });
+  io.to(channel).emit(resEvents.roomcast, {
+    roomID,
+    userID,
+    nick,
+    avatar,
+  });
+  return {needTop: true};
+}
+
+function leaveRoom(resEvents, res, socket, io) {
+  const {roomID, userID} = res;
+  const channel = `room:${roomID}`;
+
+  socket.leave(channel);
+  socket.emit(resEvents.exchange, {
+    status: 'OK',
+  });
+  io.to(channel).emit(resEvents.roomcast, {
+    roomID,
+    userID,
+  });
+  return {needTop: true};
+}
+
+function message(resEvents, res, socket, io) {
+  const {roomID} = res;
+  const channel = `room:${roomID}`;
+
+  socket.emit(resEvents.exchange, {
+    status: 'OK',
+    data: res,
+  });
+  io.to(channel).emit(resEvents.roomcast, res);
+}
+
+function searchRoomID(resEvents, res, socket) {
+  socket.emit(resEvents.exchange, {
+    status: 'OK',
+    data: res,
+  });
+}
+
+const handlers = {
+  createRoom,
+  joinRoom,
+  message,
+  leaveRoom,
+  searchRoomID,
+};
+
+const checkError = (event, request) => () => {
+  let err = false;
+  if (typeof request !== 'object') err = `${event}:request is not object`;
+  if (typeof request.exchangeID !== 'string') err = `${event}:exchange is not string`;
+  if (typeof request.data !== 'object') err = `${event}:data is not object`;
+  if (err) return Promise.reject(err);
+  return Promise.resolve();
+};
+
+const updateTop = (actions, io) => (data) => {
+  if (!data || !data.needTop) return;
   actions.getTop()
     .then((res) => {
       io.emit('broadcast:topRooms', res);
     });
 };
 
-function check(socket, name, handler) {
-  let responseEvent = `server-response:${name}@error`;
-  socket.on(`client-request:${name}`, request => {
-    return new Promise((resolve, reject) => {
-      if (typeof request !== 'object') return reject('request is not object');
+const errorHandler = (event, request, socket) => (err) => {
+  const exchangeID = request && request.exchangeID ? request.exchangeID : 'error';
 
-      const {exchangeID, data} = request;
-      if (typeof exchangeID !== 'string') return reject('exchange is not string');
-
-      responseEvent = `server-response:${name}@${exchangeID}`;
-      if (typeof data !== 'object') return reject('data is not object');
-
-      return actions[name](data)
-        .then(resolve)
-        .catch(reject);
-    })
-    .then((res) => {
-      handler(responseEvent, res);
-    })
-    .catch((err) => {
-      socket.emit(responseEvent, {
-        status: 'ERROR',
-        description: err,
-      });
-    });
+  socket.emit(`server-response:${event}@${exchangeID}`, {
+    status: 'ERROR',
+    description: err,
   });
-}
+};
 
-function onConnection(socket) {
-  check(socket, 'joinRoom', (responseEvent, res) => {
-    const { roomID } = res.room;
-    const { userID, nick, avatar } = res.identity;
-    const channel = `room:${roomID}`;
-
-    socket.join(channel);
-    socket.emit(responseEvent, {
-      status: 'OK',
-      data: res,
-    });
-    io.to(channel).emit('roomcast:joinUser', {
-      roomID,
-      userID,
-      nick,
-      avatar,
-    });
-    updateTop();
+const wrapper = (actions, socket, io) => (event) => {
+  socket.on(`client-request:${event}`, (req) => {
+    Promise.resolve()
+      .then(checkError(event, req))
+      .then(() => actions[event](req.data))
+      .then((res) => {
+        const resEvents = {
+          exchange: `server-response:${event}@${req.exchangeID}`,
+          roomcast: `roomcast:${event}`,
+        };
+        return handlers[event](resEvents, res, socket, io);
+      })
+      .then(updateTop(io))
+      .catch(errorHandler(event, req, socket));
   });
-
-  check(socket, 'leaveRoom', (responseEvent, {roomID, userID}) => {
-    const channel = `room:${roomID}`;
-
-    socket.leave(channel);
-    socket.emit(responseEvent, {
-      status: 'OK',
-    });
-    io.to(channel).emit('roomcast:leaveUser', {
-      roomID,
-      userID,
-    });
-    updateTop();
-  });
-
-  check(socket, 'message', (responseEvent, res) => {
-    const {roomID} = res;
-    const channel = `room:${roomID}`;
-
-    socket.emit(responseEvent, {
-      status: 'OK',
-      data: res,
-    });
-    io.to(channel).emit('roomcast:message', res);
-  });
-
-  check(socket, 'searchRoomID', (responseEvent, res) => {
-    socket.emit(responseEvent, {
-      status: 'OK',
-      data: res,
-    });
-  });
-
-  check(socket, 'createRoom', (responseEvent) => {
-    socket.emit(responseEvent, {
-      status: 'OK',
-    });
-  });
-
-  updateTop();
-}
+};
 
 export default function(port) {
   socketServer.listen(port, () => {
     console.log('Socket data listening on *:' + port);
   });
-  io.on('connection', onConnection);
+  globalIO.on('connection', (socket) => {
+    Object.keys(handlers)
+      .map(wrapper(pureActions, socket, globalIO));
+    updateTop(pureActions, globalIO)({needTop: true});
+  });
 }
 
