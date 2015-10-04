20f64ea43096f1f36c2e793badac5c2bb79a843a
Denis Iogansen <pofigizm@gmail.com>
2015-10-04 21:11:06 +0300

    Split middlewares from main, add promise middleware

--- client/main.js
+++ client/main.js
@@ -1,28 +1,15 @@
 import 'scss/main.scss';
 
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import createLogger from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
 
-import { writeState, readState } from './storage';
+import { readState } from './storage';
+import createStorePlus from './middleware';
 import all from './reducers';
 import App from './components/App';
 import { updateTopRooms, newMessage, joinUser, leaveUser } from './actions';
 import { reJoinRooms } from './smartActions';
 import * as transport from './transport';
-
-const loggerMiddleware = createLogger({
-  level: 'info',
-  collapsed: true,
-});
-
-const applyMiddlewares = NODE_ENV === 'production' ?
-                         applyMiddleware(thunkMiddleware, writeState) :
-                         applyMiddleware(thunkMiddleware, writeState, loggerMiddleware);
-
-const createStorePlus = applyMiddlewares(createStore);
 
 const state = readState();
 const store = createStorePlus(all, state);
