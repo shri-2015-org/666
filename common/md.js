2f6207c8f36aa4102b72496c735c818cb8035393
John Grishin <r@exah.ru>
2015-10-07 21:56:00 +0300

    Live preview visual fixes :beer:

--- common/md.js
+++ common/md.js
@@ -1,8 +1,20 @@
 import marked from 'marked';
 
 const renderer = new marked.Renderer();
-renderer.link = function parseLink( href, title, text ) {
+renderer.link = ( href, title, text ) => {
   return `<a target="_blank" href="${ href }" title="${ title }">${ text }</a>`;
+};
+
+renderer.image = ( href, title, text ) => {
+  let out = `<img src="${ href }" alt="${ text }"`;
+  if (title) {
+    out += ` title="${ title }"`;
+  }
+  if (text === 'emoji') {
+    out += ` class="md-emoji"`;
+  }
+  out += '/>';
+  return out;
 };
 
 const options = {
