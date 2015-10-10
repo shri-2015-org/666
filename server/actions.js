9e3adb2e4652c4242dfb8044e315f6d092b8f3ba
Alexey Litvinov <sullenor@yandex-team.ru>
2015-10-10 18:21:34 +0300

    joinRoom action fixed

--- server/actions.js
+++ server/actions.js
@@ -217,10 +217,10 @@ export function joinRoom({roomID, userID, secret}) {
     })
     .then( ({user, room}) => {
       room.messages = room.messages.slice(-HISTORY_COUNT);
-      resolve({
+      return {
         identity: user,
         room,
-      });
+      };
     });
 }
 
