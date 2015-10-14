586f9c20b47c646c5a3de43febb8ac554d168ec7
Denis Iogansen <pofigizm@gmail.com>
2015-10-14 12:02:57 +0300

    fix some build errors

--- build.sh
+++ build.sh
@@ -9,35 +9,3 @@ cp -r config build;
 rm -r build/server/mock;
 find build -name "*.test.js" -type f -delete;
 
-# build frontend files
-babel-node build.js;
-
-# check this docker console
-docker info || exit 1;
-
-cp docker/start.sh build/;
-
-# check anonym/mongodb exist
-docker images | grep anonym/mongodb;
-if [ $? -ne 0 ]; then
-  ( cd docker && cd mongodb && docker build -t anonym/mongodb . ) || exit 1;
-fi
-
-# check anonym/dependensy need update
-DIFF=$( diff build/package.json docker/dependensy/data/package.json );
-if [ "$DIFF" != "" ]; then
-  cp build/package.json docker/dependensy/data/package.json;
-  ( cd docker && cd dependensy && docker build -t anonym/dependensy . ) || exit 1;
-fi
-
-# check anonym/dependensy exist
-docker images | grep anonym/dependensy;
-if [ $? -ne 0 ]; then
-  ( cd docker && cd dependensy && docker build -t anonym/dependensy . ) || exit 1;
-fi
-
-# build main container
-cp docker/Dockerfile ./;
-docker build -t anonym/main .;
-rm Dockerfile;
-
